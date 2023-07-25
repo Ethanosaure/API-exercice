6937bb6385b16d2de683b2e35cb46268c84bb7ba
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-07-25 16:56:52 +0200

    adding connection and show db

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,5 +1,5 @@
 <?php
-require ('../models/model.php');
+require ('./models/model.php');
 
 class controller 
 {
--- index.php
+++ index.php
@@ -5,9 +5,11 @@ $parts = explode('/' , $_SERVER['REQUEST_URI']);
 
 var_dump($parts);
 
-if ($parts[3] != 'controllers'){
-    http_response_code(404);
-    exit();
+if ($parts[3] == 'controllers'){
+    // http_response_code(404);
+    // exit();
+    $test = new controller();
+    $test->show();
 }
 
 $id = $part[4] ?? null;
