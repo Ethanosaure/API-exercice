9cf6f9a3855c1b9f52ba4c26b6083565155ef1e3
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-08-08 10:33:45 +0200

    adding sanitize and validation for PUT method

--- index.php
+++ index.php
@@ -1,138 +1,94 @@
 <?php
-require ('controllers/controller.php');
-$base = '/API/API-exercice';
+require "controllers/controller.php";
+$base = "/API/API-exercice";
 
-$request_uri = $_SERVER['REQUEST_URI'];
+$request_uri = $_SERVER["REQUEST_URI"];
 
-$parts = str_replace($base, '' , $request_uri);
+$parts = str_replace($base, "", $request_uri);
 
-$id = $_GET['id'] ?? null;
+$id = $_GET["id"] ?? null;
 
-$method = $_SERVER['REQUEST_METHOD'];
+$method = $_SERVER["REQUEST_METHOD"];
 
-switch ($parts){
-    case '/GET?id='.$id.'':
-       if ($method === 'GET'){
-            $response = new controller ();
+switch ($parts) {
+    case "/GET?id=" . $id . "":
+        if ($method === "GET") {
+            $response = new controller();
             $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
             $response->show($id);
         }
         break;
-    case '/GET':
-        if ($method === 'GET'){
+    case "/GET":
+        if ($method === "GET") {
             $response = new controller();
             $response->showAll();
             http_response_code(200);
         }
         break;
+    case "/POST":
+        if ($method === "POST") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            if ($data["title"] && $data["body"] && $data["author"]) {
+                $title = $data["title"];
+                $body = $data["body"];
+                $author = $data["author"];
+                $title = filter_var($title, FILTER_SANITIZE_STRING);
+                $body = filter_var($body, FILTER_SANITIZE_STRING);
+                $author = filter_var($author, FILTER_SANITIZE_STRING);
+                $response = new controller();
+                $response->add($title, $body, $author);
+                http_response_code(201);
+                echo "element added";
+            } else {
+                echo "error, cannot add element, check if all the post are fill";
+                http_response_code(400);
+            }
+        }
+        break;
 
-    case '/POST':
-        if ($method === 'POST'){
-        $data = json_decode(file_get_contents('php://input'), true);
-        if ($data['title'] && $data['body'] && $data['author']){
-        $title = $data['title'];
-        $body = $data['body'];
-        $author = $data['author'];
-        $response = new controller();
-        $response->add($title, $body, $author);
-        http_response_code(201);
-        echo 'element added';
+    case "/PUT?id=" . $id . "":
+        if ($method === "PUT") {
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (
+                !isset($data["title"]) &&
+                !isset($data["body"]) &&
+                !isset($data["author"])
+            ) {
+                echo "cannot update : no correct informations provided";
+                http_response_code(400);
+                return;
+            } else {
+                $title = filter_var($data["title"], FILTER_SANITIZE_STRING) ?? "";
+                $body = filter_var($data["body"], FILTER_SANITIZE_STRING) ?? "";
+                $author = filter_var($data["author"], FILTER_SANITIZE_STRING) ?? "";
+                $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+                $pattern = '/^[A-Za-z0-9\s.,!?\'"-]+$/';
 
-        } else {
-            echo 'error, cannot add element, check if all the post are fill';
-            http_response_code(400);
+            if (
+                !preg_match($pattern, $title) ||
+                !preg_match($pattern, $body) ||
+                !preg_match($pattern, $author)
+            ){
+                echo "remove all special characters from your informations";
+                http_response_code(400);
+            } else {
+                $response = new controller();
+                $response->charliePuth($title, $author, $body, $id);
+                http_response_code(200);
+            }
         }
     }
         break;
 
-        case '/PUT?id='.$id.'':
-            if ($method === 'PUT'){
-                $data = json_decode(file_get_contents('php://input'), true);
-                if (!isset($data['title']) && !isset($data['body']) && !isset($data['author'])){
-                echo 'cannot update, check your informations';
-                http_response_code(400);
-                return;
-                }
-                else {
-                $title = $data['title'] ?? 0; 
-                $body = $data['body'] ?? 0;
-                $author = $data['author'] ?? 0;
-                $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-                $response = new controller();
-                $response->charliePuth($title, $author, $body, $id);
-                http_response_code(200); 
-                }
-            }
-            break;
-        
-        case '/DELETE?id='.$id.'':
-            if ($method === 'DELETE'){
-                $response = new controller();
-                $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
-                $response->delete($id);
-                http_response_code(200);
-            }
+    case "/DELETE?id=" . $id . "":
+        if ($method === "DELETE") {
+            $response = new controller();
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+            $response->delete($id);
+            http_response_code(200);
+        }
     default:
-       http_response_code(400);
+        http_response_code(400);
         break;
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// if ($parts[3]){
-// if ($parts[3] == 'GET'){
-//     if (isset($parts[4])){
-//     $id = $parts[4];
-//     $get = new controller();
-//     $get->show($id);
-// } else {
-//     $getAll = new controller();
-//     $getAll->showAll();
-//     }
-// }
-// if ($parts[3] == 'POST'){
-//     $post = new controller();
-//     $title = 'test';
-//     $body = 'test'; 
-//     $author = 'test';
-//     $post->add($title, $body, $author);
-//     echo 'element successfully add to db';
-// }
-// if ($parts[3] != 'GET' && $parts[3] != 'POST'){
-//     echo 'PAGE NOT FOUND';
-//     http_response_code(404);
-//     exit();
-// }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
